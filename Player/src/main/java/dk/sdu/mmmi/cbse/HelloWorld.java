ffaac8b9714e3d2ba15b2278e361b2db1f9d8d0b
Daniea1 <112690693+Daniea1@users.noreply.github.com>
2024-04-24 10:42:24 +0200

    Explaining Comments

--- Player/src/main/java/dk/sdu/mmmi/cbse/HelloWorld.java
+++ Player/src/main/java/dk/sdu/mmmi/cbse/HelloWorld.java
@@ -2,10 +2,12 @@ package dk.sdu.mmmi.cbse;
 
 import dk.sdu.mmmi.cbse.common.data.Entity;
 
-/**
- *
- * @author Emil
- */
 public class HelloWorld extends Entity {
 
 }
+
+//Two classes with same name and directory path (player and asteroid)
+//Create a Plugins folder in root directory
+//mvn clean install
+//Move either asteroids or player snapshot into plugins
+//mvn exec:exec
