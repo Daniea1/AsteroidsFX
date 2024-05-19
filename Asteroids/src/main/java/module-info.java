b8cead2a72919b3700b72df0559b078e5605e41d
Daniea1 <112690693+Daniea1@users.noreply.github.com>
2024-05-19 17:35:09 +0200

    -ll-

--- Asteroids/src/main/java/module-info.java
+++ Asteroids/src/main/java/module-info.java
@@ -6,9 +6,7 @@ import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 
 module Asteroids {
     requires Common;
-    requires CommonBullet;
     requires CommonAsteroids;
-    uses dk.sdu.mmmi.cbse.common.bullet.BulletSPI;
     provides IGamePluginService with AsteroidsPlugin;
     provides IEntityProcessingService with AsteroidsControlSystem;
     
